c9467a5f5b4a83902758ebadf7b3f4262d7d4213
kfaizi <kiansfaizi@gmail.com>
2019-07-17 14:47:24 -0700

    deleted __pycache__ junk

--- gfftest.py
+++ gfftest.py
@@ -92,8 +92,6 @@ def parseGffToDict(filename):
     print(f'Parsed gff {filename} to dict')
 
 
-parseGffToDict("extracted_R.gff")
-
 # # extract sequence of 40kb locus around found crispr to file
 # def extractSequence(crispr, record_dict, assembly_accession):
 
@@ -138,138 +136,135 @@ parseGffToDict("extracted_R.gff")
 #   return extracted_id, extracted_start, extracted_end
 
 
-# # take crispr dict, write new feature table
-# def createNewFeatureTable(crispr, record_dict, extracted_id, extracted_start, extracted_end, assembly_accession):
+# take crispr dict, write new feature table
+def createNewFeatureTable(crispr, record_dict, extracted_id, extracted_start, extracted_end, assembly_accession):
 
-#   record = record_dict[crispr['genomic_accession']]
+  record = record_dict[crispr['genomic_accession']]
 
-#   crispr_id = crispr['crispr_id']
-#   start = int(crispr['start'])
-#   end = int(crispr['end'])
-#   seq = record.seq[start-1: end]
-#   length = end - start + 1
+  crispr_id = crispr['crispr_id']
+  start = int(crispr['start'])
+  end = int(crispr['end'])
+  seq = record.seq[start-1: end]
+  length = end - start + 1
 
-#   # open original feature table
+  # open original feature table
 
+  ft_filename = assembly_accession + '_feature_table.txt'
+  try:
+    with open(genome_dir + ft_filename) as file:
+      ft = file.read()
+  except EnvironmentError as err:
+    print(f'ERROR opening feature table: {err}')
+    return None, None
 
+  # with open(genome_dir + ft_filename) as file:
+  #   ft = file.read()
 
+  ft_split = ft.split('\n')  # split feature table into rows
+  ft_split = filter(None, ft_split)  # remove empty elements
 
-#   ft_filename = assembly_accession + '_feature_table.txt'
-#   try:
-#     with open(genome_dir + ft_filename) as file:
-#       ft = file.read()
-#   except EnvironmentError as err:
-#     print(f'ERROR opening feature table: {err}')
-#     return None, None
+  newft = []  # will hold new feature table
+  protein_seqrecords = []
+  protein_nt_seqrecords = []
 
-#   # with open(genome_dir + ft_filename) as file:
-#   #   ft = file.read()
+  header_orig = ft_split[0]  # save original header
+  ft_split = ft_split[1:]  # remove header from original ft, easier to process
 
-#   ft_split = ft.split('\n')  # split feature table into rows
-#   ft_split = filter(None, ft_split)  # remove empty elements
+  for line in ft_split:  # for row in feature table
+    line_split = line.split('\t')
 
-#   newft = []  # will hold new feature table
-#   protein_seqrecords = []
-#   protein_nt_seqrecords = []
+    feature_ga = line_split[6]
+    feature_start = int(line_split[7])
+    feature_end = int(line_split[8])
 
-#   header_orig = ft_split[0]  # save original header
-#   ft_split = ft_split[1:]  # remove header from original ft, easier to process
+    if feature_ga == crispr['genomic_accession'] and \
+       feature_start >= extracted_start and \
+       feature_end <= extracted_end:  # keep rows in range and add to newft
 
-#   for line in ft_split:  # for row in feature table
-#     line_split = line.split('\t')
+      line_split.insert(9, '')  # add sequence column after start/end
+      feature_refseq = line_split[12]  # non-redundant_refseq column, e.g. WP_001205278.1
+      if len(feature_refseq) > 0:
+        fetched_header, fetched_seq = fetchProteinSeq(feature_refseq)
+        line_split[9] = fetched_seq  # write seq into seq column
+        line_split[14] += '; ' + fetched_header  # write header into name column
 
-#     feature_ga = line_split[6]
-#     feature_start = int(line_split[7])
-#     feature_end = int(line_split[8])
+        # create protein seqrecord to return
+        protein_id = '{}__{}__{}'.format(assembly_accession, crispr_id, feature_refseq)
+        protein_seqrecord = SeqRecord(
+          Seq(fetched_seq, SingleLetterAlphabet()),
+          id=protein_id,
+          name=protein_id,
+          description="""efetch header: {}, non-redundant_refseq: {}, assembly: {}, genomic_accession: {}, nt start: {}, nt end: {}, nt length: {}, product aa length: {}""".format(
+              fetched_header[1:],
+              feature_refseq,
+              assembly_accession,
+              feature_ga,
+              feature_start,
+              feature_end,
+              line_split[18],
+              line_split[19]
+            )
+        )
+        protein_seqrecords.append(protein_seqrecord)
 
-#     if feature_ga == crispr['genomic_accession'] and \
-#        feature_start >= extracted_start and \
-#        feature_end <= extracted_end:  # keep rows in range and add to newft
-
-#       line_split.insert(9, '')  # add sequence column after start/end
-#       feature_refseq = line_split[12]  # non-redundant_refseq column, e.g. WP_001205278.1
-#       if len(feature_refseq) > 0:
-#         fetched_header, fetched_seq = fetchProteinSeq(feature_refseq)
-#         line_split[9] = fetched_seq  # write seq into seq column
-#         line_split[14] += '; ' + fetched_header  # write header into name column
-
-#         # create protein seqrecord to return
-#         protein_id = '{}__{}__{}'.format(assembly_accession, crispr_id, feature_refseq)
-#         protein_seqrecord = SeqRecord(
-#           Seq(fetched_seq, SingleLetterAlphabet()),
-#           id=protein_id,
-#           name=protein_id,
-#           description="""efetch header: {}, non-redundant_refseq: {}, assembly: {}, genomic_accession: {}, nt start: {}, nt end: {}, nt length: {}, product aa length: {}""".format(
-#               fetched_header[1:],
-#               feature_refseq,
-#               assembly_accession,
-#               feature_ga,
-#               feature_start,
-#               feature_end,
-#               line_split[18],
-#               line_split[19]
-#             )
-#         )
-#         protein_seqrecords.append(protein_seqrecord)
-
-#         # create protein nt seqrecord to return
-#         protein_nt_id = '{}__{}__{}'.format(assembly_accession, crispr_id, feature_refseq)
-#         protein_nt_seqrecord = SeqRecord(
-#           record.seq[feature_start-1: feature_end],
-#           id=protein_nt_id,
-#           name=protein_nt_id,
-#           description="""**nt sequence** efetch header: {}, non-redundant_refseq: {}, assembly: {}, genomic_accession: {}, nt start: {}, nt end: {}, nt length: {}""".format(
-#               fetched_header[1:],
-#               feature_refseq,
-#               assembly_accession,
-#               feature_ga,
-#               feature_start,
-#               feature_end,
-#               line_split[18]
-#             )
-#         )
-#         protein_nt_seqrecords.append(protein_nt_seqrecord)
+        # create protein nt seqrecord to return
+        protein_nt_id = '{}__{}__{}'.format(assembly_accession, crispr_id, feature_refseq)
+        protein_nt_seqrecord = SeqRecord(
+          record.seq[feature_start-1: feature_end],
+          id=protein_nt_id,
+          name=protein_nt_id,
+          description="""**nt sequence** efetch header: {}, non-redundant_refseq: {}, assembly: {}, genomic_accession: {}, nt start: {}, nt end: {}, nt length: {}""".format(
+              fetched_header[1:],
+              feature_refseq,
+              assembly_accession,
+              feature_ga,
+              feature_start,
+              feature_end,
+              line_split[18]
+            )
+        )
+        protein_nt_seqrecords.append(protein_nt_seqrecord)
 
 
-#       # add row to new feature table
-#       joined = '\t'.join(line_split)
-#       newft.append(joined)
+      # add row to new feature table
+      joined = '\t'.join(line_split)
+      newft.append(joined)
 
-#   # add crispr summary row to newft
-#   crispr_summary = [
-#     'CRISPR array', crispr['crispr_type'], '', '', '', '', crispr['genomic_accession'], crispr['start'], crispr['end'], str(seq), '', '', '', '', crispr['crispr_id'], '', '', '', str(length), '', crispr['dr_consensus_seq']
-#   ]
-#   crispr_summary_joined = '\t'.join(crispr_summary)
-#   newft.append(crispr_summary_joined)
+  # add crispr summary row to newft
+  crispr_summary = [
+    'CRISPR array', crispr['crispr_type'], '', '', '', '', crispr['genomic_accession'], crispr['start'], crispr['end'], str(seq), '', '', '', '', crispr['crispr_id'], '', '', '', str(length), '', crispr['dr_consensus_seq']
+  ]
+  crispr_summary_joined = '\t'.join(crispr_summary)
+  newft.append(crispr_summary_joined)
 
-#   # add drs and spacer rows to newft
-#   for el in crispr['elements']:
-#     el_start = int(el['start'])
-#     el_end = int(el['end'])
-#     el_seq = record.seq[el_start-1: el_end]  # actual DR seq, not consensus
-#     el_length = el_end - el_start + 1
+  # add drs and spacer rows to newft
+  for el in crispr['elements']:
+    el_start = int(el['start'])
+    el_end = int(el['end'])
+    el_seq = record.seq[el_start-1: el_end]  # actual DR seq, not consensus
+    el_length = el_end - el_start + 1
 
-#     el_row = [
-#       el['element_type'], crispr['crispr_type'], '', '', '', '', crispr['genomic_accession'], el['start'], el['end'], str(el_seq), '', '', '', '', el['element_type'], '', '', '', str(el_length), '', ''
-#     ]
-#     el_row_joined = '\t'.join(el_row)
-#     newft.append(el_row_joined)
+    el_row = [
+      el['element_type'], crispr['crispr_type'], '', '', '', '', crispr['genomic_accession'], el['start'], el['end'], str(el_seq), '', '', '', '', el['element_type'], '', '', '', str(el_length), '', ''
+    ]
+    el_row_joined = '\t'.join(el_row)
+    newft.append(el_row_joined)
 
-#   # add header to top of newft
-#   header = header_orig.split('\t')
-#   header.insert(9, 'sequence')
-#   header_joined = '\t'.join(header)
-#   newft.insert(0, header_joined)
+  # add header to top of newft
+  header = header_orig.split('\t')
+  header.insert(9, 'sequence')
+  header_joined = '\t'.join(header)
+  newft.insert(0, header_joined)
 
-#   # write new feature table to file
-#   newft_path = '{}{}__feature_table_new.txt'.format(extracted_dir, extracted_id)
-#   with open(newft_path, 'w') as outf:
-#     newft_joined = '\n'.join(newft)
-#     outf.write(newft_joined)
+  # write new feature table to file
+  newft_path = '{}{}__feature_table_new.txt'.format(extracted_dir, extracted_id)
+  with open(newft_path, 'w') as outf:
+    newft_joined = '\n'.join(newft)
+    outf.write(newft_joined)
 
-#   print(f'Wrote new feature table to file {newft_path}')
+  print(f'Wrote new feature table to file {newft_path}')
 
-#   return protein_seqrecords, protein_nt_seqrecords
+  return protein_seqrecords, protein_nt_seqrecords
 
 
 # # fetch protein from Entrez via non-redundant_refseq
@@ -314,55 +309,53 @@ parseGffToDict("extracted_R.gff")
 #   print(f'Wrote extracted region protein NT sequences to file {path}')
 
 
+def main():
 
-# def main():
-
-#   for f in glob.glob(gff_dir + '*.gff'):
-#     # gff_filename = 'GCF_000006845-1_ASM684v1_genomic.gff'
-#     gff_filename = f.split('/')[-1]
-#     assembly_accession = gff_filename[:-len('_genomic.gff')]
-#     fna_filename = assembly_accession + '_genomic.fna'
-
+   for f in glob.glob(gff_dir + '*.gff'):
+    # gff_filename = 'GCF_000006845-1_ASM684v1_genomic.gff'
+    gff_filename = f.split('/')[-1]
+    assembly_accession = gff_filename[:-len('_genomic.gff')]
+    fna_filename = assembly_accession + '_genomic.fna'
 
 
-#     dict = parseGffToDict(gff_filename, assembly_accession)
-#     for crispr in dict['crisprs_list']:
-#       with open(genome_dir + fna_filename, 'rU') as file:
-#         record_dict = SeqIO.to_dict(SeqIO.parse(file, 'fasta'))
+    dict = parseGffToDict(gff_filename, assembly_accession)
+    for crispr in dict['crisprs_list']:
+      with open(genome_dir + fna_filename, 'rU') as file:
+        record_dict = SeqIO.to_dict(SeqIO.parse(file, 'fasta'))
 
-#       ex_id, ex_start, ex_end = extractSequence(crispr, record_dict, assembly_accession)
-#       protein_seqrecords, protein_nt_seqrecords = createNewFeatureTable(crispr, record_dict, ex_id, ex_start, ex_end, assembly_accession)
-#       createProteinFile(protein_seqrecords, ex_id)
-#       createProteinNTFile(protein_nt_seqrecords, ex_id)
-#       print('---')
+      ex_id, ex_start, ex_end = extractSequence(crispr, record_dict, assembly_accession)
+      protein_seqrecords, protein_nt_seqrecords = createNewFeatureTable(crispr, record_dict, ex_id, ex_start, ex_end, assembly_accession)
+      createProteinFile(protein_seqrecords, ex_id)
+      createProteinNTFile(protein_nt_seqrecords, ex_id)
+      print('---')
 
-# # if __name__ == "__main__":
-# #     main()
+# if __name__ == "__main__":
+#     main()
 
-# # ############################################################################
-# # import re
+# ############################################################################
+# import re
 
-# # def update_csv(gff):
-# #     dr_test = re.compile('^\>')
-# #     keyword_test = re.compile(keyword)
+# def update_csv(gff):
+#     dr_test = re.compile('^\>')
+#     keyword_test = re.compile(keyword)
 
-# #     with open(gff, 'r') as f:
-# #         line = f.readline()
-# #         while(line):
-# #             if header_test.match(line) and keyword_test.search(line):
-# #                 headers.append(line.rstrip())  # makes list of headers
-# #                 line = f.readline()
-# #                 reads.append(line.rstrip())  # makes list of corresponding reads
-# #             line = f.readline()  # exit loop
+#     with open(gff, 'r') as f:
+#         line = f.readline()
+#         while(line):
+#             if header_test.match(line) and keyword_test.search(line):
+#                 headers.append(line.rstrip())  # makes list of headers
+#                 line = f.readline()
+#                 reads.append(line.rstrip())  # makes list of corresponding reads
+#             line = f.readline()  # exit loop
 
 
-# # gffInfoFields = ["seqid",
-# #                  "source",  # always CRISPRfinder
-# #                  "type",
-# #                  "start",
-# #                  "end",
-# #                  "score",  # .
-# #                  "strand",
-# #                  "phase",  # .
-# #                  "attributes"]
+# gffInfoFields = ["seqid",
+#                  "source",  # always CRISPRfinder
+#                  "type",
+#                  "start",
+#                  "end",
+#                  "score",  # .
+#                  "strand",
+#                  "phase",  # .
+#                  "attributes"]
 
