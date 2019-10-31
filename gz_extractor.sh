cecd815ef532bcf68b969f69a8f411cfb3e07b50
kfaizi <30393862+kfaizi@users.noreply.github.com>
2019-10-31 12:49:09 -0700

    generalized gz extraction

--- gz_extractor.sh
+++ gz_extractor.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # gz_extractor.sh
-# extract NCBI WGS fasta files of form *.fsa_nt.gz
 
 src=$1 # directory with gzipped fastas
 dest=$2 # directory for unzipped fastas
@@ -9,9 +8,9 @@ mkdir -vp $dest
 
 cd $src
 
-for i in $( ls *.fsa_nt.gz ); do # for every zipped fasta in src,
+for i in $( ls *.gz ); do # for every zipped file in src,
     zipped=$i
-    gunzip -kc < $zipped > $dest/${i%%.gz} # unzip a copy and send to dest file of same name; keeps src 
+    gunzip -kc < $zipped > $dest/${i%%.gz} # unzip to dest; keeps src copy
 done
 
 exit
